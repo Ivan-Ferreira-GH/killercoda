fcd4b4317522a1821406d71284b095a9ab16f6a8
Ivan Ferreira <49401483+Ivan-Ferreira-GH@users.noreply.github.com>
2022-08-09 17:40:12 -0400

    Update foreground.sh

--- istio-fast-track/scenario1/init/foreground.sh
+++ istio-fast-track/scenario1/init/foreground.sh
@@ -5,3 +5,4 @@ echo "export PATH=/root/istio-${ISTIO_VERSION}/bin:\$PATH" >> .bashrc
 export PATH=/root/istio-${ISTIO_VERSION}/bin:$PATH
 mv /tmp/demo.yaml /root/istio-${ISTIO_VERSION}/manifests/profiles/
 istioctl install --set profile=demo -y --manifests=/root/istio-${ISTIO_VERSION}/manifests
+apt install elinks -y
